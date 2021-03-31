5af2243beeb321f6def5120f1634a6b33cf249f8
Arnaud Ritti <77437157+arnaud-ritti@users.noreply.github.com>
2021-03-31 09:15:08 +0200

    Update

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ const server = http.createServer((request, response) => {
 });
 
 server.listen(3000, () => {
-  console.log("Running at http://localhost:3000");
+  console.log("Go ! Running at http://localhost:3000");
 });
